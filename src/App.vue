74cf7d91790f6805c14b2d0f69764e6559f4a645
Cristopher <cristopherquiros2@gmail.com>
2026-02-20 16:20:35 -0600

    Save token on localStorage so the user dont need the log in again

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="app">
-    <LoginView />
+    <component :is="currentView" />
   </div>
 </template>
 
-<script>
-import LoginView from './components/loginView.vue';
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import loginView from './components/loginView.vue';
+import vehicleListView from './components/vehicleListView.vue';
 
-export default {
-  name: 'App',
-  components: {
-    LoginView
-  }
-}
+const isAuthenticated = ref(false);
+
+onMounted(() => {
+  isAuthenticated.value = !!localStorage.getItem('token');
+});
+
+const currentView = computed(() => {
+  return isAuthenticated.value ? vehicleListView : loginView;
+});
 </script>
 
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&display=swap');
+
 body {
   margin: 0;
   padding: 0;
   background-color: #050505;
+  font-family: 'Montserrat', sans-serif;
 }
 
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
