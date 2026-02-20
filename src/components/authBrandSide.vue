8f9c4445b7544cc3eb63dc4273b5408b0db34792
Cristopher <cristopherquiros2@gmail.com>
2026-02-20 16:01:49 -0600

    Slogan Title

---
+++ src/components/authBrandSide.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="brand-side">
+    <div class="brand-content">
+      <h1 class="premium-title">TICO<span>AUTOS</span></h1>
+      <p class="premium-subtitle">EXCLUSIVIDAD EN CADA KILÓMETRO</p>
+      <div class="decorative-line"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'authBrandSide'
+}
+</script>
