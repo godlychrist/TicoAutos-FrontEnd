e0e884b3da70fd33980365166928b89739dc03db
Cristopher <cristopherquiros2@gmail.com>
2026-02-20 20:07:06 -0600

    Handling saving the ID and Username on localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
-    <component :is="currentView" />
+    <loginView v-if="!isLoggedIn" @login="handleLogin" />
+    <vehicleListView v-else :username="currentUser.username" @logout="handleLogout" />
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
-import loginView from './components/loginView.vue';
-import vehicleListView from './components/vehicleListView.vue';
+import { ref } from 'vue';
+import loginView from './components/auth/loginView.vue';
+import vehicleListView from './components/vehicles/vehicleListView.vue';
 
-const isAuthenticated = ref(false);
+const isLoggedIn = ref(false);
+const currentUser = ref({ username: '' });
 
-onMounted(() => {
-  isAuthenticated.value = !!localStorage.getItem('token');
-});
+const handleLogin = (userData) => {
+  currentUser.value.username = userData.username;
+  isLoggedIn.value = true;
+  // Guardamos el objeto completo del usuario que viene del backend
+  localStorage.setItem('user', JSON.stringify(userData));
+};
 
-const currentView = computed(() => {
-  return isAuthenticated.value ? vehicleListView : loginView;
-});
+const handleLogout = () => {
+  isLoggedIn.value = false;
+  currentUser.value.username = '';
+};
 </script>
-
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&display=swap');
@@ -29,11 +34,11 @@ body {
   padding: 0;
   background-color: #050505;
   font-family: 'Montserrat', sans-serif;
+  color: white;
 }
 
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-family: 'Montserrat', sans-serif;
 }
 </style>
