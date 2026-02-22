a05322f216aa4b525bc4f23dd92f4471375d7efe
Cristopher <cristopherquiros2@gmail.com>
2026-02-21 21:42:05 -0600

    Removed from getall

--- src/assets/services/vehicleService.js
+++ src/assets/services/vehicleService.js
@@ -1,29 +1,9 @@
 import api from './api.js';
 
 const vehicleService = {
-    async getAll() {
-        try {
-            const response = await api.get('/vehicles');
-            const apiVehicles = response.data;
-            if (apiVehicles) {
-                return apiVehicles.map(v => ({
-                    ...v,
-                    name: `${v.brand} ${v.model}`,
-                    stats: v.stats || { speed: '---', range: '---', power: '---' }
-                }));
-            }
-            return [];
-        } catch (error) {
-            console.error('Error fetching vehicles:', error);
-            throw error;
-        }
-    },
+
     async create(vehicleData) {
-        const response = await api.post('/vehicles', vehicleData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
+        const response = await api.post('/vehicles', vehicleData);
         return response.data;
     },
     async update(vehicleData) {
