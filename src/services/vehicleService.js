4cc48b1dde6ae4a3d3c92f7bd6817d71149294aa
Cristopher <cristopherquiros2@gmail.com>
2026-02-22 16:57:04 -0600

    Same here, it deleted it self, so restaured

---
+++ src/services/vehicleService.js
@@ -0,0 +1,28 @@
+import api from './api';
+
+const getAll = async () => {
+    const response = await api.get('/vehicles');
+    return response.data;
+};
+
+const create = async (vehicle) => {
+    const response = await api.post('/vehicles', vehicle);
+    return response.data;
+};
+
+const update = async (id, vehicle) => {
+    const response = await api.put(`/vehicles/${id}`, vehicle);
+    return response.data;
+};
+
+const remove = async (id) => {
+    const response = await api.delete(`/vehicles/${id}`);
+    return response.data;
+};
+
+export default {
+    getAll,
+    create,
+    update,
+    remove
+};
