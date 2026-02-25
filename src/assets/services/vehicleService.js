bf9f7b268c14715f4824ed54a8ac8feee8effebc
Cristopher <cristopherquiros2@gmail.com>
2026-02-25 16:14:00 -0600

    it updates by id

--- src/assets/services/vehicleService.js
+++ src/assets/services/vehicleService.js
@@ -2,13 +2,16 @@ import api from './api.js';
 
 const vehicleService = {
 
+    async getAll() {
+        const response = await api.get('/vehicles');
+        return response.data;
+    },
     async create(vehicleData) {
         const response = await api.post('/vehicles', vehicleData);
         return response.data;
     },
-    async update(vehicleData) {
-        const id = vehicleData._id || vehicleData.id;
-        const response = await api.put(`/vehicles/${id}`, vehicleData);
+    async update(id, vehicleData) {
+        const response = await api.post(`/vehicles/${id}`, vehicleData);
         return response.data;
     },
     async delete(vehicleId) {
