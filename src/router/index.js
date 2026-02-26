0c0351327a29681295b49b532a161b50dbdec743
Cristopher <cristopherquiros2@gmail.com>
2026-02-26 16:06:45 -0600

    route fixed

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/login',
+            path: '/',
             name: 'login',
             component: loginView
         },
