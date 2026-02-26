f8ba6e7e0ef8a92ff26161dcb45ea0ba21a77926
Brian Rodríguez <brianrp19@gmail.com>
2026-02-26 16:59:16 -0600

    feat: Implement the vehicle list view component with search, filters, pagination, and a vehicle creation modal.

--- src/components/vehicles/vehicleListView.vue
+++ src/components/vehicles/vehicleListView.vue
@@ -40,6 +40,10 @@ onMounted(() => {
         <div class="logo">TICO<span>AUTOS</span></div>
       </div>
       <div class="user-info">
+        <button class="messages-nav-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
+          <span>Mensajes</span>
+        </button>
         <span class="username">Hola, {{ currentUser }}</span>
         <button class="logout-btn" @click="handleLogout">Salir</button>
       </div>
