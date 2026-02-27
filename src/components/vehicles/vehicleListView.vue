35b6bed4dbcd6b32aa2e2e3691815dab58bf2bac
Brian Rodríguez <brianrp19@gmail.com>
2026-02-27 14:14:08 -0600

    feat: add vehicle listing page with search, filters, pagination, and creation functionality.

--- src/components/vehicles/vehicleListView.vue
+++ src/components/vehicles/vehicleListView.vue
@@ -40,7 +40,7 @@ onMounted(() => {
         <div class="logo">TICO<span>AUTOS</span></div>
       </div>
       <div class="user-info">
-        <button class="messages-nav-btn">
+        <button class="messages-nav-btn" @click="$router.push('/messages')">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
           <span>Mensajes</span>
         </button>
